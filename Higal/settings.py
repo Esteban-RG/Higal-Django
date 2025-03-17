2415409a7c980e10715b9b6f243f76d102529451
Esteban-RG <ricardoespace@gmail.com>
2025-03-17 12:24:51 -0600

    Add sqlite con

--- Higal/settings.py
+++ Higal/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'Higal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
-        'NAME'    : 'pagina_restaurante',                 # <-- UPDATED line 
-        'USER'    : 'esteban',                     # <-- UPDATED line
-        'PASSWORD': '@Database23',              # <-- UPDATED line
-        'HOST'    : 'localhost',                # <-- UPDATED line
-        'PORT'    : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',  # Motor de la base de datos
+        'NAME': BASE_DIR / 'db.sqlite3',         # Ruta al archivo de la base de datos
     }
 }
 
